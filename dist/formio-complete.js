600c6c1f86db5f6410cd8bf1d12c7ee69f184189
Jannis Schönleber <joennlae@gmail.com>
2020-01-28 16:25:45 +0100

    release for frontend

--- dist/formio-complete.js
+++ dist/formio-complete.js
@@ -1,4 +1,4 @@
-/*! ng-formio v2.38.29 | https://unpkg.com/ng-formio@2.38.29/LICENSE.txt */
+/*! ng-formio v2.38.30 | https://unpkg.com/ng-formio@2.38.30/LICENSE.txt */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.formio = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(_dereq_,module,exports){
 (function (root, factory) {
   // AMD
@@ -104648,6 +104648,10 @@ module.exports = ['FormioUtils', function(FormioUtils) {
           });
         }
 
+        if (scope.component.type === 'phoneNumber'){
+          if(scope.component.inputMask) mask = [scope.component.inputMask];
+        }
+
         // Set the mask on the input element.
         if (mask) {
           scope.inputMask = mask;
@@ -104656,7 +104660,7 @@ module.exports = ['FormioUtils', function(FormioUtils) {
             mask: mask,
             showMask: (format !== 'number' && format !== 'currency' && format !== 'datetime'),
             keepCharPositions: false,
-            guide: true,
+            guide: false,
             placeholderChar: '_'
           });
         }
